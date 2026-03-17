f190c482023fd3a43a1b1b5e1a5a616c197b7c92
infosectestwin <infosectestwin@gmail.com>
2026-03-17 16:42:13 -0500

    Finalizing for deployment to PythonAnywhere

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 from app import create_app, db
 from app.models import User, Habit, HabitLog
+import os
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
+else:
+    # Production settings for PythonAnywhere
+    app.config['DEBUG'] = False
